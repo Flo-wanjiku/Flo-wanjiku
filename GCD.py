0e8037c320ff397c76bf4010c68801c77372dda8
Flo-wanjiku <90956662+Flo-wanjiku@users.noreply.github.com>
2022-06-15 21:03:42 +0300

    Add files via upload

---
+++ GCD.py
@@ -0,0 +1,7 @@
+#getting the GCD(greatest common dividor) for 2 integers
+import math
+
+int1 = int(input("enter interger1: "))
+int2 = int(input("enter interger2: "))
+
+print(math.gcd(int1, int2))
